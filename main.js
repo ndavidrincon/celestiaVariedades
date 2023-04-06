dd319a859b0c06ce15aa603e0253869eacfef02e
ndavidrincon <digitalonly.agency@gmail.com>
2023-04-06 14:48:06 -0500

    Se agrega la funcionalidad de la x para quitar elementos del carrito junto con la disminución en el contador, también se reducen un poco el codigo para que sea mas legible y corto eliminando la funcion de agregarAlCarrito y dejando el contenido de la funcion para que se ejecute al final del contenido de la funcion renderProductSelections

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
-const shoppingCartSelection = document.querySelector(".shopping-cart");
+const productsSelectionContainer = document.querySelector(".products-selection-container");
 const productDetailCloseIcon = document.querySelector(".product-detail-close");
 const burgerIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const aside = document.querySelector(".product-detail");
 const extendedProductDetail = document.querySelector(".extended-product-detail");
-const addToCartButton = document.querySelector(".add-to-cart-button")
 const cardsContainer = document.querySelector(".cards-container");
 const iconShoppingCar = document.querySelector(".icon-shopping-car");
 let numeroProductosAgregados = 0
@@ -16,7 +15,6 @@ menuEmail.addEventListener("click", toggleDesktopMenu);
 burgerIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 productDetailCloseIcon.addEventListener("click", closeProductDetailAsaid);
-addToCartButton.addEventListener("click", agregarProductoAlCarrito);
 
 
 function toggleDesktopMenu () {
@@ -70,6 +68,8 @@ function closeProductDetailAsaid () {
     extendedProductDetail.classList.add("inactive");
 }
 
+const productsInTheCar = []
+
 const productList = [];
 productList.push ({
     id: 1,
@@ -81,7 +81,7 @@ productList.push ({
 productList.push ({
     id: 2,
     name: "Tv",
-    price: 300,
+    price: 800,
     image: "https://images.pexels.com/photos/6976094/pexels-photo-6976094.jpeg?auto=compress&cs=tinysrgb&w=600"
 });
 
@@ -92,22 +92,11 @@ productList.push ({
     image: "https://images.pexels.com/photos/205421/pexels-photo-205421.jpeg?auto=compress&cs=tinysrgb&w=600"
 });
 
-
-function agregarProductoAlCarrito () {
-    console.log("Entro a la funcion");
-
-    spanCantiProducts = document.querySelector(".numeroProductosAgregados");
-    numeroProductosAgregados++
-    spanCantiProducts.innerText = numeroProductosAgregados
-}
-
-
-
 function renderProducts (products) {
 
     for ( const product of products) {
 
-        const productCard = document.createElement("div")
+        const productCard = document.createElement("div");
         productCard.classList.add('product-card');
 
         const productImg = document.createElement("img");
@@ -133,7 +122,6 @@ function renderProducts (products) {
         // productImgCart.addEventListener("click",function(){ agregarProductoAlCarrito (argumento) }); ** En caso de que necesite un arguento para la funcion que quiero relacionar o asociar, entonces deberia meter ese funcion dentro de otra para que no me genere un error como en el ejemplo
         productImgCart.addEventListener("click", () => {
             renderProductsSelections(product);
-            agregarProductoAlCarrito();
         });
 
         productInfoFigure.appendChild(productImgCart);
@@ -147,6 +135,11 @@ renderProducts(productList)
 
 function renderProductsSelections (product) {
 
+    productsInTheCar.push(product)
+
+    const shoppingCartSelection = document.createElement("div");
+    shoppingCartSelection.classList.add("shopping-cart")
+
     const figureMiniature = document.createElement("figure");
     const imgFigureMiniature = document.createElement("img");
     imgFigureMiniature.setAttribute("src", product.image);
@@ -158,7 +151,18 @@ function renderProductsSelections (product) {
     pProductPriceSelection.innerText = "$" + product.price;
 
     const imgIconClose = document.createElement("img");
+    imgIconClose.classList.add("x");
     imgIconClose.setAttribute("src", "./icons/icon_close.png");
-
+    imgIconClose.addEventListener("click", () => {
+        productsSelectionContainer.removeChild(shoppingCartSelection);
+        numeroProductosAgregados--
+        spanCantiProducts.innerText = numeroProductosAgregados
+    })
+    
     shoppingCartSelection.append(figureMiniature, pProductNameSelection, pProductPriceSelection, imgIconClose);
+    productsSelectionContainer.appendChild(shoppingCartSelection)
+
+    spanCantiProducts = document.querySelector(".numeroProductosAgregados");
+    numeroProductosAgregados++
+    spanCantiProducts.innerText = numeroProductosAgregados
 }
